5d5737a7cad006afb72950dda2b14906b36423c6
CREEPSK123 <91986856+CREEPSK123@users.noreply.github.com>
2021-10-25 02:31:46 +0200

    Add files via upload

---
+++ digitalneHodiny/Segment.java
@@ -0,0 +1,27 @@
+
+
+public class Segment {
+  private Obdlznik Obdlznik;
+    public Segment(int sirka, int vyska, int x ,int y){
+     this.Obdlznik = new Obdlznik();
+     this.Obdlznik.zmenStrany (sirka, vyska);
+     this.Obdlznik.posunVodorovne (x);
+     this.Obdlznik.posunZvisle (y);
+     
+    }
+    
+    
+    
+     public void zobraz () {
+     
+     
+     this.Obdlznik.zmenFarbu ("red");
+     
+    }
+    
+    
+    public void skry () {
+        
+        this.Obdlznik.zmenFarbu("black");
+    }
+}
